a3ad3b6c3f48e4a1a60f46c88f9c43ecf57e5734
Ignacio <ccnobe@gmail.com>
2023-03-14 18:22:05 -0600

    primer commit

---
+++ Test/src/TestB/Cliente.java
@@ -0,0 +1,19 @@
+package TestB;
+
+public class Cliente {
+
+    String nombre;
+    String numeroIdentidad;
+    String profesion;
+    
+    public class Cuenta {
+
+        double saldo;
+        int agencia;
+        int numero;
+        Cliente titular;
+    }
+
+   
+}
+
